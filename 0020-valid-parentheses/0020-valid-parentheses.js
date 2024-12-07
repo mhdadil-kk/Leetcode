e72d14831cbca12ae56eff546aad914b6cc44313
mhdadil-kk <154425207+mhdadil-kk@users.noreply.github.com>
2024-12-07 08:51:57 +0530

    Time: 7 ms (17.47%), Space: 51.4 MB (46.59%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+  let stack = []
+   const map = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+    for(let char of s){
+
+     if(map[char]){
+        stack.push(char)
+     }else{
+        if(stack.length==0||map[stack.pop()]!==char){
+            return false
+        }
+     }
+    }  
+
+    return stack.length === 0
+};
