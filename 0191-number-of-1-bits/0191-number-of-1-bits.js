48d75f6367a628ac4dc0fc86e170c0760044472c
mhdadil-kk <154425207+mhdadil-kk@users.noreply.github.com>
2024-12-09 09:15:34 +0530

    Time: 0 ms (100%), Space: 51 MB (11.38%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+     let binary = n.toString(2)
+     let count = 0
+     for(let i=0 ;i<binary.length ; i++){
+        if(binary[i]==1){
+            count ++
+        }
+     }
+     return count 
+};
