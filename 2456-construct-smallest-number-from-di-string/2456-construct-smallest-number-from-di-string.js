f3570f96e1347bddd91e53daed0cc491946c3bea
mhdadil-kk <154425207+mhdadil-kk@users.noreply.github.com>
2025-02-18 09:21:00 +0530

    Time: 0 ms (100%), Space: 48.4 MB (96.88%) - LeetHub

---
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} pattern
+ * @return {string}
+ */
+var smallestNumber = function(pattern) {
+    let stack = [];
+    let result = "";
+    let num =1
+
+    for(let i=0 ; i<=pattern.length; i++){
+        stack.push(num++);
+
+        if(i===pattern.length || pattern[i] === 'I'){
+            while(stack.length>0){
+                result += stack.pop();
+            }
+        }
+    }
+    return result
+};
