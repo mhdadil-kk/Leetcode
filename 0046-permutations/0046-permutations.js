88e8934b6e2ce3875d765b05b065464206a31425
mhdadil-kk <154425207+mhdadil-kk@users.noreply.github.com>
2025-03-20 10:20:07 +0530

    Time: 2 ms (65.36%), Space: 59.5 MB (32.43%) - LeetHub

--- 0046-permutations/0046-permutations.js
+++ 0046-permutations/0046-permutations.js
@@ -4,23 +4,28 @@
  */
 var permute = function(nums) {
     
-    let result = []
+  let result = [];
 
-    function backtrack(start){
-        if(start === nums.length){
-            result.push([...nums])
+    function backtrack(temp, used) {
+        if (temp.length === nums.length) {
+            result.push([...temp]); // Store a copy of temp
+            return;
         }
-
-    for(let i=start ; i<nums.length ; i++){
-        [nums[start],nums[i]] = [nums[i],nums[start]];
-        backtrack(start+1);
-        [nums[start],nums[i]] = [nums[i],nums[start]];
+        
+        for (let i = 0; i < nums.length; i++) {
+            if (used[i]) continue; // Skip already used numbers
+            
+            temp.push(nums[i]);
+            used[i] = true;
+            
+            backtrack(temp, used);
+            
+            temp.pop(); // Backtrack
+            used[i] = false;
+        }
     }
 
-    }
-
-    backtrack(0);
-    return result
-
+    backtrack([], Array(nums.length).fill(false));
+    return result;
 
 };
