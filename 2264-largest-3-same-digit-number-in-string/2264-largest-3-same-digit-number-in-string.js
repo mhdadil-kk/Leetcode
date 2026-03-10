61744c7481a2b6bd5d2297c0f01dad24e9fe3aca
mhdadil-kk <154425207+mhdadil-kk@users.noreply.github.com>
2026-03-10 11:04:46 +0530

    Time: 2 ms (56.13%), Space: 56.1 MB (15.48%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.js
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.js
@@ -3,28 +3,16 @@
  * @return {string}
  */
 var largestGoodInteger = function(num) {
-    let arr = []
-    for(let i=0;i<num.length ;i++){
-      let str =""
-       for(let j=i ;j<=i+2 ;j++){
-        if(num[i]===num[j]){
-          str = str+num[i]
+    let max = "";
+
+    for (let i = 0; i <= num.length - 3; i++) {
+        if (num[i] === num[i+1] && num[i] === num[i+2]) {
+            let sub = num.slice(i, i+3);
+            if (sub > max) {
+                max = sub;
+            }
         }
-       }
-       if(str.length == 3){
-         arr.push(str)
-       }
-       
     }
-     console.log(arr)
-   if(arr.length){
-    let a = Math.max(...arr).toString()
-    if(a=='0'){
-        return '000'
-    }else{
-        return a
-    }
-   }else{
-    return ""
-   }
+
+    return max;
 };
